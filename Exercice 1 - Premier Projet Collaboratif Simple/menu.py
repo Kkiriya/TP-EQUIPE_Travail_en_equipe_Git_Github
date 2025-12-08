c6424f66015e30b1265c78369d17772f72672bfc
Émile <69686396+Kkiriya@users.noreply.github.com>
2025-12-08 16:06:14 -0500

    added structure to menu.py

---
+++ Exercice 1 - Premier Projet Collaboratif Simple/menu.py
@@ -0,0 +1,15 @@
+def afficher_entrees():
+    pass
+
+def afficher_plats_principaux():
+    pass
+
+def afficher_desserts():
+    pass
+
+def main():
+    print("=== MENU DU RESTAURANT ===")
+    # Les autres ajouteront leur code ici
+
+if __name__ == "__main__":
+    main()
