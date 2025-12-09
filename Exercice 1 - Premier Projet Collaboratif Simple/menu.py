0dad364bfb948f51931b2f67c228f7c602ee4412
ammour nadjib <nadjib.ammour@gmail.com>
2025-12-09 04:23:26 -0500

    feat: ajout fonction afficher_entrees

--- Exercice 1 - Premier Projet Collaboratif Simple/menu.py
+++ Exercice 1 - Premier Projet Collaboratif Simple/menu.py
@@ -1,5 +1,19 @@
+#----------------------Affiche les entrees du menu-----------------------------------------#
 def afficher_entrees():
-    pass
+    """
+    Affiche la liste des entrées disponible dans le menu.
+    """
+    print("Voici la liste des entrées: \n")
+    entrees = [
+        "1. Salade niçoise",
+        "2. Soupe de tomate",
+        "3. Tomate mozzarella"
+    ]
+    for plat in entrees:
+        print(plat)
+
+
+    
 
 def afficher_plats_principaux():
     pass
