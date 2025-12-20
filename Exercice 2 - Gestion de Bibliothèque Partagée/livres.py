5798b2e9ff5adad35cf2c08efb6febcf466dac21
Émile <69686396+Kkiriya@users.noreply.github.com>
2025-12-20 16:07:19 -0500

    feat: Ajout du code de Bruno dans la fonction rechercher_livre

--- Exercice 2 - Gestion de Bibliothèque Partagée/livres.py
+++ Exercice 2 - Gestion de Bibliothèque Partagée/livres.py
@@ -26,12 +26,20 @@ def afficher_livres():
         print(f"{i}. {livre['titre']} ({livre['auteur']})")
 
 def rechercher_livre(titre): # Bruno
-    pass
+    recherche_titre = "Harry Potter"
+    print(f"\nRecherche par titre (contenant'{recherche_titre}')")
+    trouve_titre = False
+    for livre in livres:
+        if recherche_titre.lower() in livre['titre'].lower():
+            print(f"Titre: {livre['titre']}, Auteur: {livre['auteur']}, Année: {livre['annee']}")
+            trouve_titre = True
+    return trouve_titre
 
 def main():
     #test de la fonction ajouter
     ajouter_livre("Test", "Blank")
     print(bibliotheque)
+
 
 if __name__ == "__main__":
     main()
